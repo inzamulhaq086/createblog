c655d66afd26fda49e33dc47cbc2fda6a8493b5a
inzamulhaq086 <inzamulhaq086gmail.com>
2022-02-15 18:43:33 +0600

    blog

---
+++ public/js/custom.js
@@ -0,0 +1,34 @@
+
+                      // more free and premium Tailwind CSS components at https://tailwinduikit.com/
+                      handleClick1 = (flag) => {
+                        let icon = document.getElementById("chevronDown1");
+                        let icon2 = document.getElementById("chevronUp1");
+                        let menu = document.getElementById("menu1");
+                        if (flag) {
+                          menu.classList.toggle("hidden");
+                          icon.classList.toggle("hidden");
+                          icon2.classList.toggle("hidden");
+                        }
+                      };
+                      
+                      handleClick2 = (flag) => {
+                        let icon = document.getElementById("chevronDown2");
+                        let icon2 = document.getElementById("chevronUp2");
+                        let menu = document.getElementById("menu2");
+                        if (flag) {
+                          menu.classList.toggle("hidden");
+                          icon.classList.toggle("hidden");
+                          icon2.classList.toggle("hidden");
+                        }
+                      };
+                      
+                      handleClick3 = (flag) => {
+                        let icon = document.getElementById("chevronDown3");
+                        let icon2 = document.getElementById("chevronUp3");
+                        let menu = document.getElementById("menu3");
+                        if (flag) {
+                          menu.classList.toggle("hidden");
+                          icon.classList.toggle("hidden");
+                          icon2.classList.toggle("hidden");
+                        }
+                      };
