64de402c9926dabc8c8385eaea244ee3c085eaa1
inzamulhaq086 <inzamulhaq086gmail.com>
2022-02-23 18:22:53 +0600

    laravel brazz

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +18,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('blog', [BlogController::class, 'blog'])->name('blog');
-Route::get('blogfrom', [BlogController::class, 'blogfrom'])->name('blogfrom');
-Route::post('/createblog', [BlogController::class, 'blogstore'])->name('blogstore');
-Route::get('/copyblog/{id}', [BlogController::class, 'copyblog'])->name('copyblog');
-Route::get('/editblog/{id}', [BlogController::class, 'editblog'])->name('editblog');
-Route::post('/updateblog/{id}', [BlogController::class, 'updateblog'])->name('updateblog');
-Route::get('/deleteblog/{id}', [BlogController::class, 'deleteblog'])->name('deleteblog');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('blog', [BlogController::class, 'blog'])->name('blog');
+    Route::get('blogfrom', [BlogController::class, 'blogfrom'])->name('blogfrom');
+    Route::post('/createblog', [BlogController::class, 'blogstore'])->name('blogstore');
+    Route::get('/copyblog/{id}', [BlogController::class, 'copyblog'])->name('copyblog');
+    Route::get('/editblog/{id}', [BlogController::class, 'editblog'])->name('editblog');
+    Route::post('/updateblog/{id}', [BlogController::class, 'updateblog'])->name('updateblog');
+    Route::get('/deleteblog/{id}', [BlogController::class, 'deleteblog'])->name('deleteblog');
+
+    Route::resource('/tests', TestController::class);
+});
